cb401ce8e05d5eb86ff6a084be21d27e82826850
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-02-17 18:13:31 -0500

    Tema 3

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -1,8 +1,300 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5    
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5   
 
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 3
+      h1 Ebooks, emagazines, presentaciones, catálogos, ecards. 
+
+    .row.justify-content-center.mb-5
+      .col-lg-4.d-flex
+        p.mb-0 Muchas de las publicaciones que se conocen en la era de la impresión, como libros, revistas, catálogos entre otros, han migrado sus componentes al ámbito digital, conservando muchas de sus características esenciales y desarrollando otras funcionalidades que permiten una mayor interactividad por parte del usuario.  
+      .col-lg-3(style="background-color:#FFF4C6")
+        figure.mb-0
+          img(src='@/assets/curso/tema3/img01.png') 
+      .col-lg-5.d-flex.align-items-center(style="background-color:#E2F4F6")
+        p.mb-0 Existen diversos tipos de publicaciones que se distribuyen con diferente periodicidad, las publicaciones periódicas salen al mercado de una manera regular, ya sea diaria, semanal, quincenal o mensual; dentro de estas publicaciones se encuentran por ejemplo los libros y las revistas. <font class="color-1">Estas publicaciones sirven para difundir el conocimiento, información en diversos contextos, cultura, y noticias de interés.</font>
+
+    p.mb-5 Para comprender en profundidad algunas diferencias entre las publicaciones impresas y electrónicas, revise la siguiente tabla 2:
+
+    .row.justify-content-start
+      .col-lg-8
+        div.BorTit.mb-3         
+          p.mb-0 <b>Tabla 2</b> Impresos vs digitales
+
+    .tabla-b.color-acento-contenido.mb-5      
+      table   
+        tr(style="background-color: #F57C00")  
+          td.text-center.td-borde1  
+            b.text-white Impresos 
+          td.text-center.td-borde1     
+            b.text-white Digitales
+          td.text-center
+            b.text-white Similitudes
+        tr
+          td.td-borde1 Su base estructural, está hecha en papel o sustratos físicos. 
+          td.td-borde1 Dependen de un dispositivo tecnológico para su lectura.    
+          td Son medios de comunicación, difunden conocimientos, saberes e ideas de diversos temas.
+        tr
+          td.td-borde1 El medio impreso no permite una interrelación directa con el lector.
+          td.td-borde1 El usuario tiene una experiencia en la cual puede interactuar con muchos recursos dispuestos, editar, subrayar, colocar, etc.  
+          td Contienen textos, imágenes, fotografías entre otros. 
+        tr
+          td.td-borde1 El libro puede ser leído en cualquier momento, sin embargo, ocupa mucho espacio. 
+          td.td-borde1 Generalmente se necesita de internet, aunque algunos libros se pueden descargar y una ventaja es que se pueden llevar muchos ejemplares en un pequeño dispositivo.
+          td Los dos necesitan de un recurso físico para ser leídos.
+
+    .row.justify-content-start.mb-5(data-aos="zoom-in-up")
+      .col-lg-8
+        p.mb-5 Teniendo claras las similitudes y diferencias de los impresos y digitales, a continuación, se conocerán algunas de las principales presentaciones de publicaciones electrónicas:
+        .row.justify-content-start.mb-4
+          .col-lg-4.py-0.border(style="background-color: #FDD835")            
+            .row.justify-content-start.my-1
+              .col-lg-10
+                h3.mb-0 Ebooks
+              .col-lg-2.p-0(style="position:relative; height:10px")
+                figure.p-0.m-0.cubo-titulo
+                  img.m-0(src='@/assets/curso/cubo.svg') 
+        
+        p.mb-5 <font class="color-1">El ebook o libro en formato digital o electrónico, está diseñado para poder ser leído en cualquier dispositivo electrónico, tiene muchas similitudes con un libro impreso,</font> sin embargo, las publicaciones en medios digitales presentan las siguientes ventajas frente a las impresas:
+        ul.lista-ul--color                  
+          li 
+            i.fas.fa-square 
+            | Se pueden tener un gran número de libros o revistas en un solo dispositivo, sin el peso físico de los impresos.
+          li 
+            i.fas.fa-square 
+            | Sus contenidos son ligeros, breves y fluidos para una mejor experiencia. 
+          li 
+            i.fas.fa-square 
+            | Su distribución es de manera global e inmediata. 
+          li 
+            i.fas.fa-square 
+            | Se puede decir que son publicaciones que aportan al medio ambiente, depende del uso responsable de los aparatos, su vida útil y el reciclaje de los componentes. 
+          li 
+            i.fas.fa-square 
+            | Se pueden portar en diversos dispositivos, multipropósito.
+
+      .col-lg-4.d-flex.align-items-center
+        figure.mb-0
+          img(src='@/assets/curso/tema3/img02.png') 
+    
+    .row.justify-content-start.mb-5(data-aos="zoom-in")
+      .col-lg-4.d-flex.align-items-center
+        figure.mb-0
+          img(src='@/assets/curso/tema3/img03.png') 
+      .col-lg-8.d-flex.align-items-center
+        ul.lista-ul--color          
+          li 
+            i.fas.fa-square 
+            | El usuario puede hacer modificaciones convenientes para su uso, como el tamaño y tipos de la letra, que quiera manejar. 
+          li 
+            i.fas.fa-square 
+            | No necesita de una fuente de luz ya que la tableta o artefacto tecnológico, posee su propia fuente de luz y se puede calibrar el brillo e intensidad.
+          li 
+            i.fas.fa-square 
+            | Se permite realizar acciones en la misma pantalla, como tomar apuntes o pies de página. 
+          li 
+            i.fas.fa-square 
+            | De igual manera que los libros impresos, este tipo de publicaciones cuentan con derechos de autor y de uso, dentro de los cuales principalmente se encuentran la prohibición en la reproducción impresa y la reproducción a usuarios o dispositivos no autorizados. 
+          li 
+            i.fas.fa-square 
+            | Se puede pasar de contenido o detallar información, tocando la pantalla o algún botón de conexión con un enlace.
+          li 
+            i.fas.fa-square 
+            | Las características propias de este tipo de elementos permiten la posibilidad de insertar videos, sonidos, imágenes, animaciones, gráficos y demás, utilizando formatos determinados para cada uso. 
+          li 
+            i.fas.fa-square 
+            | Por sus características básicas los ebooks son libros que facilitan el conocimiento universal ya que pueden llegar de manera inmediata, fácil y práctica, a cualquier entorno, comunidad rural o población limitada, por ejemplo, por las condiciones geográficas. 
+          li 
+            i.fas.fa-square 
+            | Una ventaja frente a las obras impresas es la facilidad de editar, cambiar o añadir contenido al libro en cualquier momento, lo cual supone costos de ahorro.  
+
+    .row.justify-content-start.mb-5(data-aos="zoom-in-up")
+      .col-lg-6        
+        .row.justify-content-start.mb-4
+          .col-lg-8.py-0.border(style="background-color: #FDD835")            
+            .row.justify-content-start.my-1
+              .col-lg-10
+                h3.mb-0 E-Magazine - Revista electrónica
+              .col-lg-2.p-0(style="position:relative; height:10px")
+                figure.p-0.m-0.cubo-titulo
+                  img.m-0(src='@/assets/curso/cubo.svg') 
+
+        p.mb-0 La revista electrónica utiliza recursos multimedia y presenta las mismas características de la revista tradicional, se distribuye en el mercado con un periodo regular, contiene una portada, contraportada, parte editorial, índice, fotos, textos, artículos, anuncios, publicidad, ilustraciones etc. <font class="color-1">Este tipo de publicación puede ser presentada en diferentes formatos como el PDF, HTML, EPUB, entre otros. Una revista puede ser:</font> 
+      .col-lg-6.d-flex.align-items-center
+        figure.mb-0
+          img(src='@/assets/curso/tema3/img04.png') 
+
+    .row.mb-5 
+      .col-lg-3.d-flex
+        .tarjeta--boton.color-primario.p-5
+          .row.justify-content-center.mb-3
+            .col-5
+              figure
+                img.m-0(src='@/assets/curso/tema3/img05.svg')  
+          
+          p.mb-0.text-center Una ventaja sobre otros medios, las imágenes, fotografías e ilustraciones, pueden contar con una mayor calidad gráfica para el usuario. 
+      .col-lg-3.d-flex
+        .tarjeta--boton.color-primario.p-5
+          .row.justify-content-center.mb-3
+            .col-5
+              figure
+                img.m-0(src='@/assets/curso/tema3/img06.svg')  
+          
+          p.mb-0.text-center Diversa y versátil en los formatos, el lector puede observar el contenido y posteriormente imprimirlos.
+      .col-lg-3.d-flex
+        .tarjeta--boton.color-primario.p-5
+          .row.justify-content-center.mb-3
+            .col-5
+              figure
+                img.m-0(src='@/assets/curso/tema3/img07.svg')  
+          
+          p.mb-0.text-center De fácil acceso, ya que no es necesario la suscripción permanente, en la red se pueden adquirir números sueltos, o fragmentos de colecciones. 
+      .col-lg-3.d-flex
+        .tarjeta--boton.color-primario.p-5
+          .row.justify-content-center.mb-3
+            .col-5
+              figure
+                img.m-0(src='@/assets/curso/tema3/img08.svg')  
+          
+          p.mb-0.text-center De actualización permanente, por medio de los servicios de alerta el lector accede de inmediato a actualizaciones, cambios o adiciones que se pueden presentar en cualquier publicación digital. 
+    
+    
+    .row.justify-content-start.mb-5(data-aos="zoom-in")
+      .col-lg-4.d-flex.align-items-center
+        figure.mb-0
+          img(src='@/assets/curso/tema3/img09.png') 
+      .col-lg-8        
+        .row.justify-content-start.mb-4
+          .col-lg-5.py-0.border(style="background-color: #FDD835")            
+            .row.justify-content-start.my-1
+              .col-lg-10
+                h3.mb-0 Presentaciones digitales
+              .col-lg-2.p-0(style="position:relative; height:10px")
+                figure.p-0.m-0.cubo-titulo
+                  img.m-0(src='@/assets/curso/cubo.svg') 
+
+        p.mb-4 Las presentaciones digitales son productos multimedia, realizados en computador, que contienen diapositivas (slide) con información relevante de un tema en particular, casi siempre una presentación es un documento en el cual se debe incluir mucha información, por este motivo es preciso definir la información más relevante a presentar, la cual debe ser clara y concisa, utilizando la menor cantidad de textos, e integrando otros recursos como imágenes, fotografías, esquemas, videos, entre otros.
+        p.mb-4.color-1 La presentación se ayuda de una guía o índice para seguir un orden estipulado, requiere de un trabajo previo de investigación, en donde todos los datos deben ser concretos, claros y verídicos, ya que por lo general su desarrollo se enmarca especialmente en el área laboral y académica. Un ejemplo son las presentaciones que se realizan para una exposición, utilizando el programa Power Point. 
+        p.mb-0 Un elemento clave en el desarrollo de las presentaciones es la parte visual, fotografías, imágenes y videos que permiten complementar el contenido y motivan al espectador frente al tema que se está desarrollando; la comunicación multimedia ayuda a la organización de todos los componentes que conforman la presentación para facilitar su navegación y posterior divulgación, la integración de sonido conecta al participante con el contexto general de la presentación.
+
+    .row.justify-content-start.mb-5(data-aos="zoom-in-up")      
+      .col-lg-6
+        .row.justify-content-start.mb-4
+          .col-lg-5.py-0.border(style="background-color: #FDD835")            
+            .row.justify-content-start.my-1
+              .col-lg-10
+                h3.mb-0 Catálogos
+              .col-lg-2.p-0(style="position:relative; height:10px")
+                figure.p-0.m-0.cubo-titulo
+                  img.m-0(src='@/assets/curso/cubo.svg') 
+
+        p.mb-4 El catálogo es una pieza netamente comercial que utilizan las empresas para dar a conocer sus productos, bienes o servicios y con ello generar ventas. Esta pieza depende de la calidad de la imagen ya que los productos mostrados no se pueden medir, probar o tocar, solo se puede mirar.  Los catálogos de ventas electrónicos son elementos de marketing directo, son muy utilizados pues ahorran a las empresas millones de pesos en publicidad y ayudan con el cuidado del medio ambiente. 
+        b.mb-0.color-1 Las características principales para que esta pieza gráfica funcione de manera eficaz y adecuada son:        
+      .col-lg-6.d-flex.align-items-center
+        figure.mb-0
+          img(src='@/assets/curso/tema3/img10.png') 
+    
+    .row.row-cols-5.mb-5
+      .col
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .indicador--hover(v-if="indicadorTarjetaFlip")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/tema3/img11.png')})`}")
+            .tarjeta-flip__contenido.p-4   
+              p.mb-0.text-white Práctico, con poco texto y muchas imágenes de alta calidad que sean llamativas para que el usuario pueda observar, comparar y decidir su compra.
+      .col
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .indicador--hover(v-if="indicadorTarjetaFlip")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/tema3/img12.png')})`}")
+            .tarjeta-flip__contenido.p-4
+              p.mb-0.text-white Descripción de los productos, bienes o servicios que se ofertan, tiempos de entrega, promociones, cambios, devoluciones y garantías. 
+      .col
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .indicador--hover(v-if="indicadorTarjetaFlip")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/tema3/img13.png')})`}")
+            .tarjeta-flip__contenido.p-4       
+              p.mb-0.text-white Diferencial, ya que hoy en día existen muchas empresas que compiten por este mercado.
+      .col
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .indicador--hover(v-if="indicadorTarjetaFlip")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/tema3/img14.png')})`}")
+            .tarjeta-flip__contenido.p-4       
+              p.mb-0.text-white Puede tener una extensión mayor a la versión física, debido a las ventajas que ofrecen los dispositivos de salida. 
+      .col
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .indicador--hover(v-if="indicadorTarjetaFlip")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/tema3/img15.png')})`}")
+            .tarjeta-flip__contenido.p-4
+              p.mb-0.text-white Este tipo de formatos pueden incrementar el tráfico de cualquier sitio web, por sus facilidades de conexión, interactividad, pago y experiencia.
+
+    .row.justify-content-start.mb-5(data-aos="zoom-in")
+      .col-lg-4.d-flex.align-items-center
+        figure.mb-0
+          img(src='@/assets/curso/tema3/img16.png') 
+      .col-lg-8
+        .row.justify-content-start.mb-4
+          .col-lg-5.py-0.border(style="background-color: #FDD835")            
+            .row.justify-content-start.my-1
+              .col-lg-10
+                h3.mb-0 E-card
+              .col-lg-2.p-0(style="position:relative; height:10px")
+                figure.p-0.m-0.cubo-titulo
+                  img.m-0(src='@/assets/curso/cubo.svg') 
+
+        p.mb-4 Ecard o tarjeta virtual, hace referencia a una pieza gráfica principalmente utilizada hoy en día por grandes empresas de redes sociales dentro de sus contenidos, es un formato actual, estructurado y llamativo para el usuario, que se distribuye y consume de manera rápida.
+        p.mb-4 La eficiencia de los diseños en la web, incluyen las siguientes características:
+        ul.lista-ul--color               
+          li 
+            i.fas.fa-square 
+            | Las tarjetas se pueden dar la vuelta para mostrar aún más contenido.
+          li 
+            i.fas.fa-square 
+            | Se pueden doblar para resumir y desdoblar para desarrollar la información.
+          li 
+            i.fas.fa-square 
+            | Al apilarlas se ahorra espacio; se pueden agrupar y/o desplegar para ser utilizadas en el momento en el que sean requeridas.
+    
+        p.mb-0 <b class="color-1">La funcionalidad de este tipo de diseño es limitada.</b> Por tanto, no es una buena idea para la promoción y/o venta de un amplio catálogo de productos, por ejemplo. Sin embargo, representa una forma actualizada a las necesidades del usuario en línea y su tendencia al uso de dispositivos móviles.
+    
+    .row.justify-content-start.mb-5(data-aos="zoom-in-up")
+      .col-lg-7
+        p.mb-5 Se puede aprovechar su uso, si el contenido a promocionar cuenta con alguna(s) de las siguientes características:
+        ul.lista-ul--color.mb-4                  
+          li 
+            i.fas.fa-square 
+            | Si es una especie de colección que cuenta con diversos contenidos multimedia.
+          li 
+            i.fas.fa-square 
+            | Si soporta contenido de amplitud variable e interacción en línea, como una sección de comentarios o botones de enlace hacia redes sociales.
+          li 
+            i.fas.fa-square 
+            | Si el contenido condensa información clave junto con imágenes de alta resolución para hacer la navegación más dinámica y fluida.
+      
+        .tema2
+          .cajon.color-acento-contenido.p-4       
+            p.mb-0.color-1 Al igual que el consumidor promedio, el usuario de un sitio web no pretende únicamente encontrarse con opciones de productos, sino que, de manera inconsciente busca involucrarse emocionalmente con éstos. englobando esto en un concepto simple y de gran utilidad en la web, se puede decir que las emociones se comparten para que los productos sean consumidos.
+        
+        .tema3
+          .bloque-texto-d.color-secundario.p-4
+            .bloque-texto-d__texto.mb-2
+              i.fas.fa-quote-left
+              p.mb-0.text-regular Siendo así, las tarjetas representan un excelente vínculo de interacción entre el usuario y el sitio web; desde su práctico diseño rectangular, hasta su portabilidad y fácil distribución, el diseño web de tarjetas es sin duda un estilo vanguardista y de gran utilidad informativa.             
+              i.fas.fa-quote-right.text-white
+            .bloque-texto-d__autor 
+              b.mb-0 (Puente, 2015)
+              i.fas.fa-quote-right
+              
+      .col-lg-5.d-flex.align-items-center
+        figure.mb-0
+          img(src='@/assets/curso/tema3/img17.png')  
+    Separador
 </template>
 
 <script>
