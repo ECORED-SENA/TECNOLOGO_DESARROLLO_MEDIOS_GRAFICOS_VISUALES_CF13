d5a6d832831b2982dd628ee07f98f1bf5224cea7
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-02-19 10:41:53 -0500

    Ajustes actividades y sintesis

--- src/config/global.js
+++ src/config/global.js
@@ -266,12 +266,12 @@ export default {
         titulo: 'Sintesis',
         nombreRuta: 'sintesis',
       },
-      // {
-      //   nombreRuta: 'actividad',
-      //   icono: 'far fa-question-circle',
-      //   titulo: 'Actividad didáctica',
-      //   desarrolloContenidos: true,
-      // },
+      {
+        nombreRuta: 'actividad',
+        icono: 'far fa-question-circle',
+        titulo: 'Actividad didáctica',
+        desarrolloContenidos: true,
+      },
       {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
