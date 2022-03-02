0511bb3f0dbf637e440216979e2b433f873b46ce
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-03-02 18:57:11 -0500

    Ajustes

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -20,27 +20,6 @@
               a.boton.boton--b(:href="obtenerLink('/actividades/actividad1/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
-
-    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
-      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
-        .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
-          figure
-            img(src="@/assets/template/casos-uso.svg", alt="Texto que describa la imagen")
-      .col-12.col-md-8.col-lg-9
-        .titulo-segundo
-          h2 Actividad didáctica 2
-        p.mb-4 Organizar en el orden correcto las instrucciones a seguir en el algoritmo, que permite a una persona saber cuánto dinero puede retirar del cajero automático
-        .tarjeta.tarjeta--azul.p-3
-          .row.justify-content-around.align-items-center            
-            .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Conceptualización y metodologías EPUB
-            .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad2/story.html')" target="_blank")
-                span Realizar
-                i.fas.fa-puzzle-piece
-
-    
-
     
     
 
