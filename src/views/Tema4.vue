95bbc833b5845eb5ba72f4634bde6026b2caedec
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-03-03 18:53:16 -0500

    Animaciones tema 4

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -14,8 +14,11 @@
           p.mb-4 Todo proyecto nace con la necesidad de resolver, generar o ejecutar una idea en particular. Este momento de creación es llamado preproducción y comprende en este caso la creación de diferentes componentes como el guion y el <i>storyboard</i>. <b class="color-1">Esta fase permite reconocer cuáles herramientas y recursos serán necesarios, para el desarrollo del proyecto en general.</b> 
           p.mb-0 La siguiente figura 1 presenta los pasos a seguir en preproducción, de un proyecto gráfico, desde el inicio del proyecto hasta las decisiones y acciones tomadas previas a su ejecución. 
       .col-lg-5.d-flex.align-items-center
-        figure.mb-0
+        .imagen-adornos-flotantes
           img(src='@/assets/curso/tema4/img01.png') 
+          .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:; right:  0% ; top: ; bottom: 0% ; width: 90px ")
+          .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:  0% ; right:; top:0% ; bottom: ; width: 60px ")
+          .imagen-adornos-flotantes__img: img(src="@/assets/curso/float3.png" style="left:  -5% ; right: ; top: ; bottom: -5% ; width: 60px ")
 
     figure.mb-5
       img(src='@/assets/curso/tema4/img02.png') 
@@ -30,43 +33,61 @@
               h3.mb-5.color-1 Necesidad
               p.mb-0 La necesidad se encuentra enmarcada en la carencia o falta de algo, a partir de allí crear un nuevo proyecto editorial respondiendo a los interrogantes: <font class="color-1">¿qué?, ¿para quién?, ¿cuándo?, ¿cuánto?, ¿cómo?, ¿en qué medio?, etc.</font>
             .col-lg-6
-              figure(data-aos="zoom-in-up")
+              .imagen-adornos-flotantes
                 img(src='@/assets/curso/tema4/img03.png') 
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:; right:  5% ; top: ; bottom: 8% ; width: 90px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:  2% ; right:; top:2% ; bottom: ; width: 60px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float3.png" style="left:  5% ; right: ; top: ; bottom: 20% ; width: 60px ")
           .row.mb-0
             .col-lg-6
               h3.mb-5.color-1 Requerimientos
               p.mb-0 Impuestos a partir de las necesidades identificadas, y que son establecidas por parte del cliente, contexto, política, editorial u otra que demande el proyecto en particular. 
             .col-lg-6
-              figure(data-aos="zoom-in-up")
+              .imagen-adornos-flotantes
                 img(src='@/assets/curso/tema4/img04.png') 
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:; right:  5% ; top: ; bottom: 8% ; width: 90px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:  2% ; right:; top:2% ; bottom: ; width: 60px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float3.png" style="left:  5% ; right: ; top: ; bottom: 20% ; width: 60px ")
           .row.mb-0
             .col-lg-6
               h3.mb-5.color-1 Célula
               p.mb-0 Equipo de expertos interdisciplinarios para el desarrollo del proyecto en cada etapa de su diseño, ejecución y validación.
             .col-lg-6
-              figure(data-aos="zoom-in-up")
+              .imagen-adornos-flotantes
                 img(src='@/assets/curso/tema4/img05.png') 
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:; right:  5% ; top: ; bottom: 8% ; width: 90px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:  2% ; right:; top:2% ; bottom: ; width: 60px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float3.png" style="left:  5% ; right: ; top: ; bottom: 20% ; width: 60px ")
           .row.mb-0
             .col-lg-6
               h3.mb-5.color-1 Sesiones creativas
               p.mb-0 Las sesiones creativas son fundamentales para el desarrollo de nuevas ideas, estrategias, metodologías y puesta en marcha del proyecto. Allí confluye todo el equipo de expertos y nacen las mejores ideas. Para que estas sesiones sean productivas, es necesario implementar un planning con reuniones periódicas y actividades definidas para cada persona o grupos de trabajo. Una estrategia que se puede implementar es la lluvia de ideas.
             .col-lg-6
-              figure(data-aos="zoom-in-up")
+              .imagen-adornos-flotantes
                 img(src='@/assets/curso/tema4/img06.png') 
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:; right:  5% ; top: ; bottom: 8% ; width: 90px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:  2% ; right:; top:2% ; bottom: ; width: 60px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float3.png" style="left:  5% ; right: ; top: ; bottom: 20% ; width: 60px ")
           .row.mb-0
             .col-lg-6
               h3.mb-5.color-1 <i>Brainstorming</i> o lluvia o tormenta de ideas
               p.mb-0 Esta herramienta generalmente es utilizada cuando se busca llegar a consensos o tomar decisiones en grupo.<br><br>El brainstorming potencia los procesos creativos, y la generación de alternativas originales que ayudan en la resolución de una situación o problema.
             .col-lg-6
-              figure(data-aos="zoom-in-up")
+              .imagen-adornos-flotantes
                 img(src='@/assets/curso/tema4/img07.png') 
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:; right:  5% ; top: ; bottom: 8% ; width: 90px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:  2% ; right:; top:2% ; bottom: ; width: 60px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float3.png" style="left:  5% ; right: ; top: ; bottom: 20% ; width: 60px ")
           .row.mb-0
             .col-lg-6
               h3.mb-5.color-1 Software
               p.mb-0 Los programas y aplicaciones son de vital importancia en la creación de cualquier proyecto digital. Es necesario contar con programas de diseño, interfaces y el manejo de plataformas y formatos requeridos, para la ejecución de cada proyecto editorial.
             .col-lg-6
-              figure
+              .imagen-adornos-flotantes
                 img(src='@/assets/curso/tema4/img08.png') 
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:; right:  5% ; top: ; bottom: 8% ; width: 90px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:  2% ; right:; top:2% ; bottom: ; width: 60px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float3.png" style="left:  5% ; right: ; top: ; bottom: 20% ; width: 60px ")
           .row.mb-0
             .col-lg-6
               h3.mb-5.color-1 Validación
@@ -86,15 +107,21 @@
                   i.fas.fa-square 
                   | Tiene en cuenta el feedbacks o retroalimentación de los clientes con la empresa y de los integrantes de los equipos de trabajo.
             .col-lg-6
-              figure(data-aos="zoom-in-up")
+              .imagen-adornos-flotantes
                 img(src='@/assets/curso/tema4/img09.png') 
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:; right:  5% ; top: ; bottom: 8% ; width: 90px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:  2% ; right:; top:2% ; bottom: ; width: 60px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float3.png" style="left:  5% ; right: ; top: ; bottom: 20% ; width: 60px ")
           .row.mb-0
             .col-lg-6
               h3.mb-5.color-1 Producción
               p.mb-0 La pre producción se puede decir que finaliza en este paso, es acá cuando la planificación y todos los pasos anteriores conjugan para que se puedan hacer las creaciones pactadas. Acá se establecen las últimas acciones para dar inicio a la fase de producción, y se prepara lo último para establecer y revisar los cronogramas, para tener buenos resultados.
             .col-lg-6
-              figure(data-aos="zoom-in-up")
+              .imagen-adornos-flotantes
                 img(src='@/assets/curso/tema4/img10.png')     
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:; right:  5% ; top: ; bottom: 8% ; width: 90px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float5.png" style="left:  2% ; right:; top:2% ; bottom: ; width: 60px ")
+                .imagen-adornos-flotantes__img: img(src="@/assets/curso/float3.png" style="left:  5% ; right: ; top: ; bottom: 20% ; width: 60px ")
     
 
 </template>
